6c69ed8d30795508a4858b9dd946fb8e6bdbfd5b
NotTom31 <thomasmezzie@gmail.com>
2024-08-03 13:06:46 -0700

    game manager base

---
+++ Assets/Code/GameManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum GameState
+{
+    MAIN_MENU,
+    PAUSE,
+    DRAW,
+    CONTRACT,
+    BET,
+    PLAY
+}
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    public delegate void StateChangeHandler(GameState state);
+    public static event StateChangeHandler OnStateChange;
+
+    public GameState gameState { get; private set; } = GameState.MAIN_MENU;
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(Instance);
+        }
+    }
+
+        public void SetState(GameState state)
+    {
+        gameState = state;
+        OnStateChange?.Invoke(state); //prob a better way to do this
+    }
+}
